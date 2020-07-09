8d2fd1e647002aac68ef2f71f46a71e546af46bb
Steven Snyder <sts12589@gmail.com>
2020-07-09 11:55:27 -0400

    added button feature for cat and dog facts to be randomly generated and appended to page

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 //   .then(response => response.json())
 //   .then(data => console.log(data));
 
-  fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=dog&amount=10')
+function addMoreFacts() {
+
+  fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=cat,dog&amount=12')
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -14,9 +16,18 @@
    for (let i = 0; i < data.length; i++) {
        const element = data[i];
        console.log(element.text)
+       $('.grid-container').prepend(
+        `<div class="card align-items-center">
+       <div class="card-body">
+          <h5 class="card-title align-items-center">${element.type}</h5>
+         <p class="card-text">${element.text}</p>
+        </div>
+      </div>`
+       )
        
    }
   })
   .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
   });
+}
